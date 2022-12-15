aab52478ec69ac4e75aecc2943f2b0b2a7931b7f
Esther White <esteewhite@hotmail.com>
2022-12-15 08:41:28 +0200

    they are loading

--- pm-vanilla-js/app.js
+++ pm-vanilla-js/app.js
@@ -7,49 +7,50 @@ const searchBtn = document.getElementById("searchBtn");
 
 searchBtn.addEventListener("click", () => {
     let myFilteredArray = [];
-	const createdDiv = document.createElement("div");
-	container.appendChild(createdDiv);
+	// const createdDiv = document.createElement("div");
+	// container.appendChild(createdDiv);
 
 	fetch("https://fakerapi.it/api/v1/products")
 		.then((response) => response.json())
 		.then((data) => {
-			if (!input.value) {
-				createdDiv.innerText = "No products Faked yet..";
-				myFilteredArray = [];
-			} else if ((input.value = 1)) {
-				myFilteredArray.push(data.data[0]);
-                let description = myFilteredArray.map(el => el.description);
-                let image = myFilteredArray.map(el => el.image);
-                createdDiv.innerHTML = `${description} <img src="${image}" alt="">`;
-			} 
-            // else if ((input.value = 2)) {
+
+			data.data.forEach(createBox);
+
+			// if (!input.value) {
+			// 	createdDiv.innerText = "No products Faked yet..";
+			// 	myFilteredArray = [];
+			// } else if (input.value == 1) {
 			// 	myFilteredArray.push(data.data[0]);
-			// 	myFilteredArray.push(data.data[1]);
             //     let description = myFilteredArray.map(el => el.description);
-            //     // createdDiv.innerHTML = description;
+            //     let image = myFilteredArray.map(el => el.image);
+            //     createdDiv.innerHTML = `${description} <img src="${image}" alt="">`;
+			// } 
+            // else if ((input.value == 2)) {
+			// 	myFilteredArray.push(data.data[0,1]);
+            //     let description = myFilteredArray.map(el => el.description);
+            //     createdDiv.innerHTML = description;
             //     console.log(myFilteredArray);
-			// } else {
+			// } 
+			// else {
             //     createdDiv.innerText = "No!";
             // }
 		});
 
 		clear ();
-
-	// if (!input.value) {
-	// 	createdDiv.innerText = "No products Faked yet..";
-	// 	myFilteredArray = [];
-	// } else if ((input.value = 1)) {
-	// 	createdDiv.innerHTML = `<div>${myFilteredArray}</div>`;
-	// } else {
-	// 	createdDiv.innerHTML = `<div>${myFilteredArray}</div>`;
-	// }
-
-	// for (const container of document.querySelectorAll(".container")) {
-	// 	if (container.querySelectorAll("div").length > 0) {
-	// 		container.removeChild(container.firstChild);
-	// 	}
-	// }
 });
+
+// Create boxes
+function createBox(data) {
+	const box = document.createElement("div");
+  
+	const { image, description } = data;
+	// instead of using item.image & item.text
+  
+	box.classList.add("box");
+	box.innerHTML = `<img src="${image}" alt="${description}"/>`;
+	  container.appendChild(box);
+  }
+
 
 function clear () {
 	for (const container of document.querySelectorAll(".container")) {
