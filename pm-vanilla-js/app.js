8695e71128e39bbb65e3c27ce8b4d68a4379046d
Esther White <esteewhite@hotmail.com>
2022-12-15 15:11:30 +0200

    first version

--- pm-vanilla-js/app.js
+++ pm-vanilla-js/app.js
@@ -1,7 +1,6 @@
 const input = document.querySelector("input");
 const container = document.querySelector(".container");
-// const thumbsWrapper = document.querySelector(".thumbs-wrapper");
-const mainImage = document.querySelector(".main-image");
+const thumbsWrapper = document.querySelector(".thumbs-wrapper");
 const search = document.querySelector(".search");
 const searchBtn = document.getElementById("searchBtn");
 
@@ -9,7 +8,6 @@ searchBtn.addEventListener("click", () => {
 	fetch("https://fakerapi.it/api/v1/products")
 		.then((response) => response.json())
 		.then((items) => {
-
 			if (!input.value || input.value == 0) {
 				container.innerHTML = `
 				<div class="message">
@@ -21,34 +19,34 @@ searchBtn.addEventListener("click", () => {
 			// maybe switch statements for here
 			else if (input.value == 1) {
 				const oneItem = items.data.slice(0, 1);
-				// oneItem.forEach(createBox);
+				oneItem.forEach(createBox);
 			} else if (input.value == 2) {
 				const twoItems = items.data.slice(0, 2);
-				// twoItems.forEach(createBox);
+				twoItems.forEach(createBox);
 			} else if (input.value == 3) {
 				const threeItems = items.data.slice(0, 3);
-				// threeItems.forEach(createBox);
+				threeItems.forEach(createBox);
 			} else if (input.value == 4) {
 				const fourItems = items.data.slice(0, 4);
-				// fourItems.forEach(createBox);
+				fourItems.forEach(createBox);
 			} else if (input.value == 5) {
 				const fiveItems = items.data.slice(0, 5);
 				console.log(fiveItems);
-				// fiveItems.forEach(createBox);
+				fiveItems.forEach(createBox);
 			} else if (input.value == 6) {
 				const sixItems = items.data.slice(0, 6);
 				sixItems.forEach(createBox);
 			} else if (input.value == 7) {
 				const sevenItems = items.data.slice(0, 7);
-				// sevenItems.forEach(createBox);
+				sevenItems.forEach(createBox);
 			} else if (input.value == 8) {
 				const eightItems = items.data.slice(0, 8);
-				// eightItems.forEach(createBox);
+				eightItems.forEach(createBox);
 			} else if (input.value == 9) {
 				const nineItems = items.data.slice(0, 9);
 				nineItems.forEach(createBox);
 			} else if (input.value == 10) {
-				// items.data.forEach(createBox);
+				items.data.forEach(createBox);
 			} else {
 				container.innerHTML = `
 				<div class="message">
@@ -57,86 +55,31 @@ searchBtn.addEventListener("click", () => {
 				</div>
 				`;
 			}
-
-			items.data.forEach((object) => {
-				const box = document.createElement("div");
-				const thumbsWrapper = document.createElement("div");
-
-				thumbsWrapper.classList.add("thumbs");
-
-				const { image, name, price, tags } = object;
-
-				box.classList.add("box");
-				box.innerHTML = `
-				<img class="main-image" src="${image}" alt="artificial product"/>
-					<p class="product-name">"${name}"</p>
-					<p class="product-price">"${price}"</p>
-					<p class="product-tags">"${tags}"</p>			
-				`;
-	
-				box.appendChild(thumbsWrapper);
-				container.appendChild(box);
-
-				object.images.forEach((image) => {
-					const imgElement = document.createElement("img");
-					imgElement.src = image.url;
-					thumbsWrapper.appendChild(imgElement);
-				});
-			});
 		});
 
 	clear();
 });
 
 // Create boxes
-// function createBox(items) {
-// 	const box = document.createElement("div");
+function createBox(items) {
+	const box = document.createElement("div");
 
-// 	// const thumbnail = items.images.forEach(image => image.url)
-// 	// console.log(thumbnail);
+	const { image, name, price, tags } = items;
 
-// 	const { image, name, price, tags } = items;
-// 	// instead of using item.image & item.text
-
-// 	box.classList.add("box");
-// 	box.innerHTML = `
-// 	<img class="mainImage" src="${image}" alt="artificial product"/>
-// 		<div class="thumbs-wrapper">
-// 		</div>
-// 		<div class="product-info">
-// 		<p class="product-name">"${name}"</p>
-// 		<p class="product-price">"${price}"</p>
-// 		<p class="product-tags">"${tags}"</p>
-// 		</div>
+	box.classList.add("box");
+	box.innerHTML = `
+	<img class="mainImage" src="${image}" alt="artificial product"/>
+		<div class="thumbs-wrapper">
+		</div>
+		<div class="product-info">
+		<p class="product-name">"${name}"</p>
+		<p class="product-price">"${price}"</p>
+		<p class="product-tags">"${tags}"</p>
+		</div>
 	
-// 	`;
-// 	container.appendChild(box);
-
-// 	items.data.forEach((object) => {
-// 		object.images.forEach((image) => {
-// 			const imgElement = document.createElement("img");
-// 			imgElement.src = image.url;
-// 			box.appendChild(imgElement);
-// 		});
-// 	});
-// }
-
-//   function createThumbs(thumbsArr) {
-
-// 	items.data.forEach(thumb => {
-// 		const thumbsArr = thumb.images;
-// 		}
-// 	);
-
-// 	  const thumbs = document.createElement("img");
-// 	  const thumbsWrapper = document.querySelector(".thumbs-wrapper");
-
-// 	  thumbs.classList.add("thumb");
-
-// 	  thumbsWrapper.appendChild(thumbs);
-// 	  thumbs.outerHTML = `<img class="thumb-test" src="${images}" />`
-
-//   }
+	`;
+	container.appendChild(box);
+}
 
 // Clear loaded products
 function clear() {
